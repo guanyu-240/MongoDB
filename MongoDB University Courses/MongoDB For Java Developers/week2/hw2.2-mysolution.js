bc1df8cb60e2af2176acc6963cad54998cda6d52
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-01-19 19:33:25 -0500

    Create hw2.2-mysolution.js

---
+++ MongoDB University Courses/MongoDB For Java Developers/week2/hw2.2-mysolution.js
@@ -0,0 +1,8 @@
+var mycursor = db.grades.find({'type':'homework'}).sort({'student_id':1, 'score':1, });
+while (mycursor.hasNext()) { 
+    cur = mycursor.next(); 
+    if (sid != cur['student_id']) {
+        db.grades.remove({'_id':cur['_id']})  
+    } 
+    sid=cur['student_id'];   
+}
