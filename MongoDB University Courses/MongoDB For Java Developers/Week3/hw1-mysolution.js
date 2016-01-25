d505dc1d650e558d4dcdd7fe8b5b0a2b3d5892d2
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-01-25 16:59:46 -0500

    Delete hw1-mysolution.js

--- MongoDB University Courses/MongoDB For Java Developers/Week3/hw1-mysolution.js
+++
@@ -1,7 +0,0 @@
-var my_cursor = db.students.find({"scores.type": "homework"});
-my_cursor.forEach(function(s){
-  var scores_new = s.scores.sort(function(sc1, sc2){
-    return sc1.score < sc2.score && sc1.type==="homework" && sc2.type==="homework"}); 
-    scores_new.pop(); 
-    db.students.update( { _id: s._id }, { $set: {scores: scores_new} } );
-  });
