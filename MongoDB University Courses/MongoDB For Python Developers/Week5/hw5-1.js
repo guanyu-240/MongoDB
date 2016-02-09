1c23f9d623efeb5da071750d758f544a8cefba67
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-02-09 10:45:15 -0500

    Create hw5-1.js

---
+++ MongoDB University Courses/MongoDB For Python Developers/Week5/hw5-1.js
@@ -0,0 +1,8 @@
+db.posts.aggregate([
+  {"$unwind":"$comments"}, 
+  {"$group":
+    {"_id":"$comments.author", "count":{$sum:1}}}, 
+  {"$sort":{"count":-1}}, 
+  {"$limit":1}, 
+  {"$project":
+    {_id:0, "author":"$_id", count:1}}])
